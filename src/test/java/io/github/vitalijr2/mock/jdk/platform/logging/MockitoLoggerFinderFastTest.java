ca8a6274599fa4225e9f7212fca052ff7c10005a
Vitalij Berdinskih <vitalij_r2@outlook.com>
2024-07-15 19:09:15 +0300

    Initial import

---
+++ src/test/java/io/github/vitalijr2/mock/jdk/platform/logging/MockitoLoggerFinderFastTest.java
@@ -0,0 +1,52 @@
+package io.github.vitalijr2.mock.jdk.platform.logging;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
+import static org.hamcrest.collection.IsMapWithSize.aMapWithSize;
+import static org.hamcrest.core.Is.isA;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+
+import java.lang.System.Logger;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("fast")
+class MockitoLoggerFinderFastTest {
+
+  @DisplayName("Create and add logger")
+  @Test
+  void createLogger() {
+    // given
+    var loggers = new HashMap<String, Logger>();
+    var loggerFinder = new MockitoLoggerFinder(loggers);
+
+    // when
+    loggerFinder.getLogger("test", getClass().getModule());
+
+    // then
+    assertAll("Logger was created", () -> assertThat("size", loggers, aMapWithSize(1)),
+        () -> assertThat("entry", loggers, hasEntry(equalTo("test"), isA(Logger.class))));
+  }
+
+  @DisplayName("Reuse existing logger")
+  @Test
+  void reuseExistingLogger() {
+    // given
+    var loggers = new HashMap<>(Map.of("test", mock(Logger.class)));
+    var loggerFinder = new MockitoLoggerFinder(loggers);
+
+    // when
+    loggerFinder.getLogger("test", getClass().getModule());
+
+    // then
+    assertAll("Logger was reused", () -> assertThat("size", loggers, aMapWithSize(1)),
+        () -> assertThat("entry", loggers, hasEntry(equalTo("test"), isA(Logger.class))));
+    ;
+  }
+
+}
