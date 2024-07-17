59ccd5df5473307a0bcdab67f33ead4e7e7265ab
Vitalij Berdinskih <vitalij_r2@outlook.com>
2024-07-17 10:33:44 +0300

    Add a demo integration test
    * Add a demo integration test
    
    * Codacy: ignore the demo
    
    * CodeNarc: ignore the demo
    
    * Update README

---
+++ src/it/hello-world/src/test/java/example/hello/HelloServiceFullTest.java
@@ -0,0 +1,63 @@
+package example.hello;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringStartsWith.startsWith;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.clearInvocations;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import java.lang.System.Logger;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class HelloServiceFullTest {
+
+  private static Logger logger;
+
+  @BeforeAll
+  static void setUpClass() throws Exception {
+    logger = System.getLogger("HelloService");
+  }
+
+  @BeforeEach
+  void setUp() throws Exception {
+    clearInvocations(logger);
+  }
+
+  @DisplayName("Names")
+  @ParameterizedTest(name = "<{0}>")
+  @ValueSource(strings = {"John", "Jane"})
+  void names(String name) {
+    var helloService = new HelloService();
+
+    assertDoesNotThrow(() -> helloService.sayHello(name));
+
+    var logger = System.getLogger("HelloService");
+
+    verify(logger).log(System.Logger.Level.INFO, "Hello " + name + "!");
+    verifyNoMoreInteractions(logger);
+  }
+
+  @DisplayName("Null or empty name")
+  @ParameterizedTest(name = "<{0}>")
+  @NullAndEmptySource
+  @ValueSource(strings = "   ")
+  void nullOrEmptyName(String name) {
+    var helloService = new HelloService();
+
+    var exception = assertThrows(RuntimeException.class, () -> helloService.sayHello(name));
+
+    verifyNoInteractions(System.getLogger("HelloService"));
+
+    assertThat(exception.getMessage(), startsWith("Name is"));
+  }
+
+
+}
